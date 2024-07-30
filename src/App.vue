63797edcb237586787e345b18a2e6360c8af1ce3
Antonbinom <protest_the_hero@mail.ru>
2024-07-30 22:13:06 +0300

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3 class="text-3xl">Заголовок Формы</h3>
+    <div class="col-3 bg-primary">
+      <div class="flex align-items-center justify-content-between mb-5">
+        <label class="text-xl" for="input-1">Название инпута</label>
+        <InputNumber
+          id="input-1"
+          v-model="value"
+          :minFractionDigits="1"
+          :maxFracionDigits="2"
+          :useGrouping="false"
+          min="0"
+          max="10"
+        />
+      </div>
+      <div class="flex align-items-center justify-content-between mb-5">
+        <label class="text-xl" for="input-1">Название инпута</label>
+        <InputNumber
+          id="input-1"
+          v-model="value"
+          :minFractionDigits="1"
+          :maxFracionDigits="2"
+          :useGrouping="false"
+          min="0"
+          max="10"
+        />
+      </div>
+      <div class="flex align-items-center justify-content-between mb-5">
+        <label class="text-xl" for="input-1">Название инпута</label>
+        <InputNumber
+          id="input-1"
+          v-model="value"
+          :minFractionDigits="1"
+          :maxFracionDigits="2"
+          :useGrouping="false"
+          min="0"
+          max="10"
+        />
+      </div>
+      <div class="flex align-items-center justify-content-between mb-5">
+        <label class="text-xl" for="input-1">Название инпута</label>
+        <InputNumber
+          id="input-1"
+          v-model="value"
+          :minFractionDigits="1"
+          :maxFracionDigits="2"
+          :useGrouping="false"
+          min="0"
+          max="10"
+        />
+      </div>
+      <div class="flex align-items-center justify-content-between">
+        <label class="text-xl" for="input-1">Название инпута</label>
+        <SelectButton v-model="select" :options="options" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/lara-dark-teal/theme.css";
+import "primeflex/primeflex.css";
+import InputNumber from "primevue/inputnumber";
+import SelectButton from "primevue/selectbutton";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    InputNumber,
+    SelectButton,
+  },
+  data() {
+    return {
+      value: "",
+      select: "Wi-Fi",
+      options: ["Wi-Fi", "Modem"],
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* background: #000; */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(PrimeVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
